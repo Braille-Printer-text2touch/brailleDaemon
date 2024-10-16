82aa3d445841c6d87fe2d561171a7b108f0b7637
kenny5033 <dazrerx@gmail.com>
2024-10-16 08:07:48 -0400

    added pipe communications

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from time import sleep
 from adafruit_motorkit import MotorKit
+import os
+
+PIPE_PATH = "/var/run/user/1000/text2type_pipe"
 
 kit = MotorKit()
 
-for i in range(10):
-    kit.stepper1.onestep()
-    print("Stepping one step on motor 1")
-    sleep(1)
+# Set up pipe
+try:
+    os.mkfifo(PIPE_PATH)
+except FileExistsError:
+    print("Pipe already exists, carrying on as normal")
+except OSError as e:
+    print("Error creating pipe:", e)
+finally:
+    print("Pipe ready at " + PIPE_PATH)
+
+while True:
+    # have to keep opening the pipe because the connection closes
+    # after all writers are done
+    with open(PIPE_PATH, "r") as pipe:
+            print(pipe.read())
+
+os.remove(PIPE_PATH)
+
+# example code to drive motors
+# for i in range(10):
+#     kit.stepper1.onestep()
+#     print("Stepping one step on motor 1")
+#     sleep(1)
 
